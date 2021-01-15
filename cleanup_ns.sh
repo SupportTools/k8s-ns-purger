4d6570d8877a6c27d3e5f120f5282b667a51bd2d
Matthew Mattox <matthew.mattox@rancher.com>
2021-01-14 20:08:25 -0600

    Importing

---
+++ cleanup_ns.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ -z $PURGE_AGE ]]
+then
+  PURGE_AGE="8 hour"
+fi
+
+echo "App Label: $APP_LABEL"
+echo "Purge Age: $PURGE_AGE"
+
+kubectl get ns -l app="$APP_LABEL",ns-purge=true -o go-template --template '{{range .items}}{{.metadata.name}},{{.metadata.creationTimestamp}}{{"\n"}}{{end}}' | while IFS=, read -r namespace timestamp
+do
+  echo "#################################################################"
+  echo "Namespace: $namespace"
+  echo "Timestamp: $timestamp"
+  deletetime=`date -d "$PURGE_AGE ago" -Ins --utc | sed 's/+0000/Z/'`
+  if [[ "$timestamp" < "$deletetime" ]]
+  then
+    echo "Need to delete"
+    if [[ -z $namespace ]]
+    then
+      echo "Namespace is empty for some reason"
+    else
+      echo "Deleteing..."
+      kubectl delete ns "$namespace"
+    fi
+  fi
+  echo "#################################################################"
+done
